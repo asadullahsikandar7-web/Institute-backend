4020371f054626a6cba145803d31d6ea43129714
asad ullah <asadullahsikandar7@glowingsoft.com>
2026-05-08 21:26:30 +0500

    backend vercel ready

--- server.js
+++ server.js
@@ -60,9 +60,4 @@ app.use((req, res) => {
   });
 });
 
-// Server
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
