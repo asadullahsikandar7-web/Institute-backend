f079d26b6abb86bad92ff323144791b8293ebf70
asad ullah <asadullahsikandar7@glowingsoft.com>
2026-05-21 18:04:14 +0500

    fixed9 name

--- src/routes/classRoute.js
+++ src/routes/classRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Class from "../models/ClassModel.js";
+import Class from "../models/classModel.js";
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
